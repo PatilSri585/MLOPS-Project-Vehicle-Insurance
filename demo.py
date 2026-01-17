f0a62716ddd152f14f95eafba45968596e33798f
PatilSri585 <sri.patil321@gmail.com>
2026-01-18 00:00:55 +0530

    logging and exception tested and added

---
+++ demo.py
@@ -0,0 +1,19 @@
+# # below code is to check the logging config
+# from main.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+# # below code is to check the exception config
+from main.logger import logging
+from main.exception import MyException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
