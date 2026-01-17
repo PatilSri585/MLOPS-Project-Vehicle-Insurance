b655ed120c75c2d01ebccb8f234b8b6baa1df15a
PatilSri585 <sri.patil321@gmail.com>
2026-01-17 17:27:04 +0530

    package added to repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="main",
+    version="0.0.1",
+    author="Sri Harsha Valmoori",
+    author_email="valmooriharsha1994@gmail.com",
+    packages=find_packages()
+)
